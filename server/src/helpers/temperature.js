12be80b4b90ca07f149534e88587620ed5b9d2b8
Szabolcs Gergye <szabolcs.gergye@botium.at>
2021-12-08 17:11:50 +0100

    log ds18b20 devices

--- server/src/helpers/temperature.js
+++ server/src/helpers/temperature.js
@@ -3,6 +3,14 @@ const { delay } = require('./util')
 
 const RETRY_COUNT = 3
 
+sensor.list((err, deviceIds) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(deviceIds)
+  }
+})
+
 const readCurrentTemperature = async () => {
   let tempC = null
   for (let i = 0; i < RETRY_COUNT && !tempC; i++) {
