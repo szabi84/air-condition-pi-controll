f291406ce44dd2162a5ed7c09564172aa4e504d9
Szabolcs Gergye <szabolcs.gergye@botium.at>
2022-01-29 20:02:25 +0100

    add client and database

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 require('debug').enable('*')
 const debug = require('debug')('app')
 const express = require('express')
@@ -13,7 +14,14 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../../client/build')))
+
 app.use('/', routes)
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, '../../client/build/index.html')
+  )
+})
 
 database.initialize().then(() => {
   hvacController.run()
